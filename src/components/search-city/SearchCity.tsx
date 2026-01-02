17523ee9428e86b1e6fe81b43a98c0056be65159
Nergis <nergisrahimzade123@gmail.com>
2026-01-01 17:28:51 -0800

    Updated css of OutputCity and added weather icons

--- src/components/search-city/SearchCity.tsx
+++ src/components/search-city/SearchCity.tsx
@@ -28,67 +28,90 @@ export function SearchCity() {
     //const html = use(<OutputCity origin={origin} destination={destination} />);
   };
 
+  const handleReset = () => {
+    setShowOutput(false);
+    setOrigin('');
+    setDestination('');
+    setMode('');
+  };
+
   return (
-    <div className='search-container'>
-      <FormControl className='search-select-container' fullWidth>
-        <InputLabel id='origin-select-label'>From...</InputLabel>
-        <Select
-          className='search-select-item'
-          labelId='simple-select-label'
-          id='simple-select'
-          value={origin}
-          label='Select City'
-          onChange={handleOriginChange}
-        >
-          {cities.map((city) => {
-            return (
+    <div>
+      <div className='search-container'>
+        <FormControl className='search-select-container' fullWidth>
+          <InputLabel id='origin-select-label'>From...</InputLabel>
+          <Select
+            className='search-select-item'
+            labelId='simple-select-label'
+            id='simple-select'
+            value={origin}
+            label='Select City'
+            onChange={handleOriginChange}
+          >
+            {cities.map((city) => {
+              return (
+                <MenuItem key={city.cityName} value={city.cityName}>{city.cityName}</MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+
+        <FormControl className='search-select-container' fullWidth>
+          <InputLabel id='destination-select-label'>To...</InputLabel>
+          <Select
+            className='search-select-item'
+            labelId='simple-select-label'
+            id='simple-select'
+            value={destination}
+            label='Select City'
+            onChange={handleDestinationChange}
+          >
+            {cities.map((city) => (
               <MenuItem key={city.cityName} value={city.cityName}>{city.cityName}</MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
+            ))}
+          </Select>
+        </FormControl>
 
-      <FormControl className='search-select-container' fullWidth>
-        <InputLabel id='destination-select-label'>To...</InputLabel>
-        <Select
-          className='search-select-item'
-          labelId='simple-select-label'
-          id='simple-select'
-          value={destination}
-          label='Select City'
-          onChange={handleDestinationChange}
+        <FormControl className='search-select-container' fullWidth>
+          <InputLabel id='mode-select-label'>By...</InputLabel>
+          <Select
+            className='search-select-item'
+            labelId='simple-select-label'
+            id='simple-select'
+            value={mode}
+            label='Select Mode'
+            onChange={handleModeChange}
+          >
+            <MenuItem value='foot'>Foot</MenuItem>
+            <MenuItem value='car'>Car</MenuItem>
+            <MenuItem value='plane'>Plane</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          onClick={handleClick}
         >
-          {cities.map((city) => (
-            <MenuItem key={city.cityName} value={city.cityName}>{city.cityName}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+          Calculate
+        </Button>
 
-      <FormControl className='search-select-container' fullWidth>
-        <InputLabel id='mode-select-label'>By...</InputLabel>
-        <Select
-          className='search-select-item'
-          labelId='simple-select-label'
-          id='simple-select'
-          value={mode}
-          label='Select Mode'
-          onChange={handleModeChange}
+        <Button
+          onClick={handleReset}
         >
-          <MenuItem value='foot'>Foot</MenuItem>
-          <MenuItem value='car'>Car</MenuItem>
-          <MenuItem value='plane'>Plane</MenuItem>
-        </Select>
-      </FormControl>
+          Reset
+        </Button>
 
-      <Button
-        onClick={handleClick}
-      >
-        Calculate
-      </Button>
 
-      {showOutput && (
-        <OutputCity origin={origin} destination={destination} mode={mode} />
-      )}
+      </div>
+
+      <div className='result-container'>
+        {showOutput && (
+          <OutputCity origin={origin} destination={destination} mode={mode} />
+        )}
+      </div>
+
+      <div>
+
+      </div>
     </div>
   );
 }
